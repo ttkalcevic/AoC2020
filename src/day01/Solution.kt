3ef4430867176c129125cba4757157590af4cb6b
Tomislav Tkalcevic <tomislav.tkalcevic@decode.agency>
2020-12-02 12:09:53 +0100

    Fixed typo in solution B output print

--- src/day01/Solution.kt
+++ src/day01/Solution.kt
@@ -7,6 +7,7 @@ import common.readFileAsIntList
 const val PUZZLE_INPUT_PATH = "./src/day01/PuzzleInput.txt"
 const val DESIRED_RESULT = 2020
 
+@Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
     val puzzleInput = readFileAsIntList(PUZZLE_INPUT_PATH)
     solutionA(puzzleInput)
@@ -22,5 +23,5 @@ private fun solutionA(list: List<Int>) {
 private fun solutionB(list: List<Int>) {
     val combinations = CombinationGenerator(list, 3)
     val result = combinations.filter { combination -> combination.sum() == DESIRED_RESULT }
-    println("Solution A: ${result.first()} -> ${result.first().multiply()}")
+    println("Solution B: ${result.first()} -> ${result.first().multiply()}")
 }
